42703ae13f65c733390d2087f7ad421381239dff
Pedrinho75king <pedroyan@colegiopaulodetarso.com.br>
2026-03-24 14:31:31 +0000

    hi

--- script.js
+++ script.js
@@ -3,5 +3,21 @@ const taskList = document.querySelector("#taskList")
 const taskInput = document.querySelector("#taskInput")
 
 taskBtn.addEventListener("click", function(){
-    alert("CLIQUEI !!!")
+    const li = document.createElement('li');
+    const taskInputValue = taskInput.value;
+     if (taskInputValue == ''){
+        return
+    }
+    const span = document.createElement('span');
+    span.textContent = taskInputValue;
+    const button = document.createElement('button');
+    button.textContent = 'Excluir'
+    button.addEventListener('click', function(){
+        li.remove();
+    })
+
+    li.appendChild(span)
+    li.appendChild(button)
+
+    taskList.appendChild(li);
 })
